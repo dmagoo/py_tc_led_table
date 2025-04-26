df5e0d9b4232e33b54f9223657316f7b3e6c5bb4
David MacGugan <dmagoo@users.noreply.github.com>
2025-04-25 22:04:09 -0400

    feat: windows compatibility

--- src/bootstrap_bindings.py
+++ src/bootstrap_bindings.py
@@ -5,8 +5,12 @@ import platform
 def setup_paths():
     project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     bindings_dir = os.path.join(project_root, "cpplib", "python_bindings")
+
     if platform.system() == "Windows":
         bindings_dir = os.path.join(bindings_dir, "Release")
+        if hasattr(os, 'add_dll_directory'):
+            os.add_dll_directory(bindings_dir)
 
     if bindings_dir not in sys.path:
         sys.path.insert(0, bindings_dir)
+    print("bindings_dir =", bindings_dir)
